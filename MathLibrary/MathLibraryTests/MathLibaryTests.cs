16029c0310dd22da8b50e830c8d7984c49835f1e
Jennifer Wesselman <wesselman.j@gmail.com>
2018-02-20 16:21:41 -0500

    first commit

---
+++ MathLibrary/MathLibraryTests/MathLibaryTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLibrary;
+using System;
+
+namespace MathematicsLibraryTests
+{
+    [TestClass()]
+    public class MathematicsLibraryTests
+    {
+        private MathLib mathlib;
+        private double errorFactor;
+
+        [TestInitialize]
+        public void Initialization()
+        {
+            mathlib = new MathLib();
+            errorFactor = 0.0000001;
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.AreEqual(4.5, mathlib.Add(1.5, 3.0), errorFactor);
+            Assert.AreEqual(9, mathlib.Add(4.5, 4.5), errorFactor);
+            Assert.AreEqual(1.4, mathlib.Add(2.7, -1.3), errorFactor);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            Assert.AreEqual(4.2, mathlib.Subtract(7.4, 3.2), errorFactor);
+            Assert.AreEqual(-7.8, mathlib.Subtract(-9.3, -1.5), errorFactor);
+            Assert.AreEqual(23.8, mathlib.Subtract(20.1, -3.7), errorFactor);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Assert.AreEqual(23.68, mathlib.Multiply(7.4, 3.2), errorFactor);
+            Assert.AreEqual(13.95, mathlib.Multiply(-9.3, -1.5), errorFactor);
+            Assert.AreEqual(-74.37, mathlib.Multiply(20.1, -3.7), errorFactor);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Assert.AreEqual(2.3125, mathlib.Divide(7.4, 3.2), errorFactor);
+            Assert.AreEqual(6.2, mathlib.Divide(-9.3, -1.5), errorFactor);
+            Assert.AreEqual(-5.43243243243243, mathlib.Divide(20.1, -3.7), errorFactor);
+        }
+
+        [TestMethod()]
+        public void SquareTest()
+        {
+            Assert.AreEqual(54.76, mathlib.Square(7.4), errorFactor);
+            Assert.AreEqual(86.49, mathlib.Square(-9.3), errorFactor);
+            Assert.AreEqual(404.01, mathlib.Square(20.1), errorFactor);
+        }
+
+        [TestMethod()]
+        public void InverseTest()
+        {
+            Assert.AreEqual(0.135135135135135, mathlib.Inverse(7.4), errorFactor);
+            Assert.AreEqual(-0.10752688172043, mathlib.Inverse(-9.3), errorFactor);
+            Assert.AreEqual(0.0497512437810945, mathlib.Inverse(20.1), errorFactor);
+        }
+
+        [TestMethod()]
+        public void NegateTest()
+        {
+            Assert.AreEqual(-7.4, mathlib.Negate(7.4), errorFactor);
+            Assert.AreEqual(9.3, mathlib.Negate(-9.3), errorFactor);
+            Assert.AreEqual(-20.1, mathlib.Negate(20.1), errorFactor);
+        }
+    }
+}
---
+++ MathLibrary/MathLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathLibraryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MathLibraryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ca01598-38b8-4972-8a9d-ba4a6bb46491")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
